df072fc18915b140753ff568b2437ac6130fc0be
athul <athul8720@gmail.com>
2019-09-09 22:11:56 +0530

    New statics

--- porter/settings.py
+++ porter/settings.py
@@ -126,9 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'upload')
 # Extra lookup directories for collectstatic to find static files
-""" STATICFILES_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-) """
+)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
