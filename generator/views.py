c51c77d1ce49739b021291184238216a602e42fc
athul <athul8720@gmail.com>
2019-09-09 22:09:28 +0530

    New Auth in Alogin :shield:

--- generator/views.py
+++ generator/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import user
 from .serializers import UserSerializer
-# Create your views here.
-class UserAPI(viewsets.ModelViewSet):
+from django.contrib.auth.mixins import LoginRequiredMixin# Create your views here.
+
+class UserAPI(LoginRequiredMixin,viewsets.ModelViewSet):
+    login_url = '/login/'
     queryset=user.objects.all()
     serializer_class=UserSerializer
