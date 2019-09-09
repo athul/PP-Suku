e3836edd6117992de6e9426b1f754cf0b2931770
athul <athul8720@gmail.com>
2019-09-09 22:55:26 +0530

    --amend

--- porter/settings.py
+++ porter/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'stats')
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'upload')
 # Extra lookup directories for collectstatic to find static files
