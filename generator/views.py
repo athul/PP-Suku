27775949cb974cf52017409fc925a5e60b13c6bf
athul <athul8720@gmail.com>
2019-09-09 22:57:53 +0530

    --amend

--- generator/views.py
+++ generator/views.py
@@ -4,7 +4,6 @@ from .models import user
 from .serializers import UserSerializer
 from django.contrib.auth.mixins import LoginRequiredMixin# Create your views here.
 
-class UserAPI(LoginRequiredMixin,viewsets.ModelViewSet):
-    login_url = 'accounts/login/'
+class UserAPI(viewsets.ModelViewSet):
     queryset=user.objects.all()
     serializer_class=UserSerializer
