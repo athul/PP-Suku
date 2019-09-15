10affe0eefc67b119147ec00193befeaa753a319
Athul Cyriac Ajay <athul8720@gmail.com>
2019-09-15 19:56:10 +0530

    Moved the Whole Frontend to React :tada: (#2)
    * Made as A React frontend
    
    * Improvs
    
    * React Finals

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+//import UserData from './components/Userdata';
+import User from './components/user'
+class App extends Component {
+    render() {
+        return (
+            <User />
+        )
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },]
+    },
+/*     module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      }, */
+}
