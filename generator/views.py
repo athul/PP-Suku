9ee2bd7fd19489fa0f0999ee714bc2739acaa951
athul <athul8720@gmail.com>
2019-11-23 16:02:01 +0530

    Removed DRF completely

--- generator/views.py
+++ generator/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets,permissions
 from .models import user
 from .forms import userForm
-from .serializers import UserSerializer
+#from .serializers import UserSerializer
 from django.contrib.auth.mixins import LoginRequiredMixin# Create your views here.
 
-class UserAPI(viewsets.ModelViewSet):
+""" class UserAPI(viewsets.ModelViewSet):
     queryset=user.objects.all()
     serializer_class=UserSerializer
-    permission_classes=(permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes=(permissions.IsAuthenticatedOrReadOnly,) """
 
 def index(request):
     return render(request, 'generator/index.html')
--- porter/settings.py
+++ porter/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "generator",
-    "rest_framework",
+    #"rest_framework",
     "frontend",
     "corsheaders",
     "graphene_django",
